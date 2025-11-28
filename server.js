38245500647a2294a4f18dd6f6c3e01dfc10c482
Gabriel Chacon <2024342@student.cct.ie>
2025-11-28 10:50:59 +0000

    Added initial CSV reading capacity

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 
 const port = 3000
+
 const express = require("express")
+const fs = require("fs")
+const csv = require("csv-parser")
+
+const filePath = "person_info.csv"
+const firstNames = []
+const lastNames = []
+const emails = []
+const ages = []
+
 const app = express()
+const stream = fs.createReadStream(filePath)
+    .pipe(csv())
+    .on("data", (row) => {
+        firstNames.push(row.first_name)
+        lastNames.push(row.last_name)
+        emails.push(row.email)
+        ages.push(parseInt(row.age))
+    })
+    .on("end", () => {
+        console.log("Reading done")
+    })
 
 app.set("view engine", "ejs")
 app.use(express.urlencoded({extended: false}))
